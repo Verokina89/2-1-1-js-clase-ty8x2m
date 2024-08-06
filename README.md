c57acf96cbd9ad41b63482e1e8bff8e9e1752cb7
Verokina89 <v.isabel1989@gmail.com>
2024-08-06 20:55:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2-1-1-js-clase-ty8x2m
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Verokina89/2-1-1-js-clase-ty8x2m)
---
+++ script.js
@@ -0,0 +1,13 @@
+// Declara tres variables utilizando let o const con diferentes tipos de datos: un número, una cadena de caracteres y un booleano.
+let numero = 5
+const texto = 'esto es un texto';
+let boolean = true;
+
+// Muestra en la consola el contenido de cada variable utilizando console.log().
+console.log('variable numero:', numero);
+console.log('variable texto', texto);
+consolo.log('variable truefalse' trufalse
+
+// Intenta hacer cambios en cada una de las variables con otros valores y tipos.
+numero = 8;
+console.log('variable numero:' numero);
